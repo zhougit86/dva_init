ded36e6fe9ffc2e82ccc30a966fa9ca71b384f37
zhou xiaogang <zhouxg2@lenovo.com>
2017-07-01 10:35:31 +0800

    use delay from redux-saga

--- src/components/Count/counts.js
+++ src/components/Count/counts.js
@@ -13,7 +13,7 @@ const CountApp = ({count, dispatch}) => {
       <div className={styles.current}>{count.current}</div>
       <div className={styles.button}>
         <button onClick={() => {
-          dispatch({type: 'count/add'});
+          dispatch({type: 'count/add',value:3});
         }}>+
         </button>
       </div>
@@ -25,7 +25,9 @@ const CountApp = ({count, dispatch}) => {
 function mapStateToProps(state) {
   return {count: state.count};
 }
-const HomePage = connect(mapStateToProps)(CountApp);
+const HomePage = connect(
+  mapStateToProps
+)(CountApp);
 
 
 export default HomePage
--- src/models/models.js
+++ src/models/models.js
@@ -2,6 +2,7 @@
  * Created by Administrator on 2017-06-15.
  */
 
+import {delay} from 'redux-saga'
 
 let countModel = {
   namespace: 'count',
@@ -10,31 +11,31 @@ let countModel = {
     current: 0,
   },
   reducers: {
-    add(state) {
-      const newCurrent = state.current + 1;
+    add(state,action) {
+      const newCurrent = state.current + action.value;
       return {
         ...state,
         record: newCurrent > state.record ? newCurrent : state.record,
         current: newCurrent,
       };
     },
-    minus(state) {
-      return {...state, current: state.current - 1};
+    minus(state,action) {
+      return {...state, current: state.current - action.value};
     },
   },
 
   effects: {
     *add(action, {call, put}) {
-      yield call(delay, 1000);
-      yield put({type: 'minus'});
+      yield call(delay, 2000);
+      yield put({type: 'minus',value:action.value});
     },
   },
 };
 
-function delay(timeout) {
-  return new Promise(resolve => {
-    setTimeout(resolve, timeout);
-  });
-}
+// function delay(timeout) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, timeout);
+//   });
+// }
 
 export  {countModel}
